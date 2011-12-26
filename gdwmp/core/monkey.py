996702c901ac001cf937756533f10b0835b98a91
Laurent Lasudry <lasudry@gmail.com>
2011-12-26 04:11:16 +0000

    Fix collection search in NL for portlet This refs #3897

--- gdwmp/core/monkey.py
+++ gdwmp/core/monkey.py
@@ -3,6 +3,7 @@ from zope.component import queryUtility, getMultiAdapter
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.vocabularies.catalog import parse_query, ParseError, itertools
 
 from gdwmp.core.browser.translations import translateId
 
@@ -47,3 +48,28 @@ def all_events_link(self):
     portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
     lang = portal_state.language()
     return translateId('ateliers', lang)
+
+
+def search(self, query_string):
+    query = self.base_query.copy()
+    if query_string == '':
+        if self.default_query is not None:
+            query.update(parse_query(self.default_query, self.portal_path))
+        else:
+            return []
+    else:
+        query.update(parse_query(query_string, self.portal_path))
+
+    # Fix :
+    query['Language'] = 'all'
+
+    try:
+        results = (x.getPath()[len(self.portal_path):] for x in self.catalog(**query))
+    except ParseError:
+        return []
+
+    if query.has_key('path'):
+        path = query['path']['query'][len(self.portal_path):]
+        if path != '':
+            return itertools.chain((path, ), results)
+    return results
